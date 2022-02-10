7bf0f6e3029da2a917646c0797c572622e2c423e
yair-hb <aythb@hotmail.com>
2022-02-10 15:18:37 -0600

    se agregan comentarios a ciertas lineas para conocer su funcion

--- movementDetect.py
+++ movementDetect.py
@@ -13,20 +13,25 @@ while True:
         break 
     gris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    #dibujamos un rectangulo en framepara señalar el estado (cuando se detecta movimiento)
     cv2.rectangle(frame,(0,0),(frame.shape[1],40),(0,0,0),-1)
     color = (0,255,0)
     texto_estado = 'Estado: no se ha detectado movimiento'
 
+    #especificamos los puntos del area a analizar
     area_pts = np.array([[240,320],[480,320],[620,frame.shape[0]],[60,frame.shape[0]]])
 
+    #determinamos el area a analizar en la que se detectara el movimiento
     imAux = np.zeros(shape=(frame.shape[:2]), dtype=np.uint8)
     imAux = cv2.drawContours(imAux, [area_pts],-1,(255),-1)
     image_area = cv2.bitwise_and(gris,gris, mask=imAux)
 
+    #obtenemos la imagen binaria donde el color blanco indica movimiento
     fgmask = mog.apply(image_area)
     fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
     fgmask = cv2.dilate(fgmask, None, iterations=2)
-
+    
+    #se encuentran los contornos en fgmask para determinar si hay movimiento
     cnts = cv2.findContours(fgmask, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
     for cnt in cnts:
         if cv2.contourArea(cnt)>500:
@@ -34,6 +39,8 @@ while True:
             cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
             texto_estado = "Estado: Alerta de Movimiento!"
             color = (0,0,255)
+    
+    #visualizamos el contorno del area que vamos a analizar y el estado de la deteccion
     cv2.drawContours(frame,[area_pts],-1,color,2)
     cv2.putText(frame, texto_estado,(10,30),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,color,1)
 
